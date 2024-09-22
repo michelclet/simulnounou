6dfb8fbc0cfeb26208ba0465634bb2f202e90474
mclet <michel.clet@descartesunderwriting.com>
2024-09-22 11:48:49 +0200

    reorganize features and display aides

--- home.py
+++ home.py
@@ -8,21 +8,27 @@ def brut2net(brut):
 
 
 # INPUTS
-c1_input, c2_input, c3_input, c4_input = st.columns(4)
-part_garde = c1_input.number_input("Part de garde", value=50, min_value=0, max_value=100)
+c1_input, c2_input = st.columns(2)
+option_annee = c1_input.selectbox(
+    ("Année de garde (change le crédit d'impôt)"),
+    ("1ère année", "2ème année ou plus"))
 
-salaire_brut_heure = c2_input.number_input("Salaire brut / heure", value=12.75)
+part_garde = c2_input.number_input("Part de garde avec la co-famille (%)", value=50, min_value=0, max_value=100)
+
+c1_input, c2_input = st.columns(2)
+salaire_brut_heure = c1_input.number_input("Salaire brut / heure (€)", value=13.44)
 salaire_net_heure = brut2net(salaire_brut_heure)
 # salaire_net_heure = math.ceil(salaire_net_heure * 100)/100
-c2_input.text(f"net / heure: {math.ceil(salaire_net_heure * 100)/100} €")
+c1_input.text(f"net / heure: {math.ceil(salaire_net_heure * 100)/100} €")
 
 st.write("")
-n_heure_hebdo = c3_input.number_input("Heures / semaine", value=48, min_value=0, max_value=50)
+n_heure_hebdo = c2_input.number_input("Nombre d'heures/semaine pour la nounou", value=48, min_value=0)
 
-part_couple = c4_input.number_input("Répartition dans le couple", value=50, min_value=0, max_value=100)
-c4_input.text(f"{part_couple}/{100-part_couple}")
+# part_couple = c4_input.number_input("Répartition dans le couple", value=50, min_value=0, max_value=100)
+# c4_input.text(f"{part_couple}/{100-part_couple}")
 
 # COMPUTE
+part_couple = 50
 part_couple /= 100
 part_garde /= 100
 part_garde = round(part_garde, 2)
@@ -52,8 +58,7 @@ if n_heure_hebdo > seuil_heure_maj_50:
     n_heure_hebdo_maj_50 = n_heure_hebdo - seuil_heure_maj_50    
     n_heure_mois_maj_50 = math.ceil(n_heure_hebdo_maj_50*52/12)
 if n_heure_hebdo > seuil_heure_max:
-    st.error("Le nombre d'heure effectives par semaine ne peut pas dépasser 50")
-    st.stop()
+    st.warning("Le nombre d'heure effectives travaillé par la nounou est supérieur à 50h")
 
 
 # Salaires brut et net
@@ -105,7 +110,11 @@ depenses_mois = salaire_net_mois_verse + charges - aides - exo_cot_heure_sup
 depenses_an = depenses_mois*12
 
 # Montant du crédit d'impôt: 50% plaffoné à 12k€ (15k€ la 1ère année) pour 1 enfant
-seuil_credit_impot = 12000
+if option_annee == "1ère année":
+    seuil_credit_impot = 15000
+else:
+    seuil_credit_impot = 12000
+
 montant_credit = 50/100 * min(depenses_an, seuil_credit_impot)
 depenses_apres_credit = depenses_an - montant_credit
 
@@ -114,7 +123,7 @@ if depenses_an > seuil_credit_impot:
     depenses_apres_seuil = depenses_an - seuil_credit_impot
 
 # Summary heures
-columns = ["Co-Famille 1 (nous)", "Co-Famille 2", "Auxilaire"]
+columns = ["Co-Famille 1", "Co-Famille 2", "Nounou"]
 data = {
     "Nombre d'heures non majorées": [
         math.ceil(n_heure_mois_no_maj*part_garde),
@@ -135,7 +144,7 @@ data = {
 df_heures = pd.DataFrame.from_dict(data, orient='index', columns=columns)
 
 # Summary salaires
-columns = ["Co-Famille 1 (nous)", "Co-Famille 2", "Auxilaire"]
+columns = ["Co-Famille 1", "Co-Famille 2", "Auxilaire"]
 data = {
     "Salaire brut (€)": [
         round(salaire_brut_mois, 2),
@@ -152,7 +161,12 @@ data = {
         round(navigo, 2),
         round(navigo*2, 2)
         ],
-    (f"Salaire net versé (dont exo cot heures sup {exo_cot_heure_sup}€ (€)"): [
+    (f"Salaire net versé sans exo. cot. heures sup {exo_cot_heure_sup} (€)"): [
+        round(salaire_net_mois_verse - exo_cot_heure_sup, 2),
+        round(salaire_net_mois_verse_restant - exo_cot_heure_sup, 2),
+        round(salaire_net_mois_verse + salaire_net_mois_verse_restant - exo_cot_heure_sup, 2)
+        ],
+    (f"Salaire net versé dont exo. cot. heures sup {exo_cot_heure_sup} (€)"): [
         round(salaire_net_mois_verse, 2),
         round(salaire_net_mois_verse_restant, 2),
         round(salaire_net_mois_verse + salaire_net_mois_verse_restant, 2)
@@ -167,15 +181,15 @@ data = {
         round(depenses_mois, 2),
         round(depenses_an, 2)
         ],
-    (f"Dépenses effective part {int(100*part_couple)}% couple (€)"): [
-        round(part_couple*depenses_mois, 2),
-        round(part_couple*depenses_an, 2)
-        ],
-    (f"Dépenses effective part {int(100-100*part_couple)}% couple (€)"): [
-        round((1-part_couple)*depenses_mois, 2),
-        round((1-part_couple)*depenses_an, 2)
-        ],
-    "Montant du crédit d'impôt (€)": [
+    # (f"Dépenses effective part {int(100*part_couple)}% couple (€)"): [
+    #     round(part_couple*depenses_mois, 2),
+    #     round(part_couple*depenses_an, 2)
+    #     ],
+    # (f"Dépenses effective part {int(100-100*part_couple)}% couple (€)"): [
+    #     round((1-part_couple)*depenses_mois, 2),
+    #     round((1-part_couple)*depenses_an, 2)
+    #     ],
+    "Crédit d'impôt 50% plafonné à 12k€ (15k€ la 1ère année) pour 1 enfant": [
         round((montant_credit)/12, 2),
         round((montant_credit), 2)
         ],
@@ -183,14 +197,14 @@ data = {
         round(depenses_apres_credit/12, 2),
         round(depenses_apres_credit, 2)
         ],
-    (f"Dépenses mensuelle réelles part {int(100*part_couple)}% couple (€)"): [
-        round(part_couple*depenses_apres_credit/12, 2),
-        round(part_couple*depenses_apres_credit, 2)
-        ],
-    (f"Dépenses mensuelle réelles part {int(100-100*part_couple)}% couple (€)"): [
-        round((1-part_couple)*depenses_apres_credit/12, 2),
-        round((1-part_couple)*depenses_apres_credit, 2)
-        ]
+    # (f"Dépenses mensuelle réelles part {int(100*part_couple)}% couple (€)"): [
+    #     round(part_couple*depenses_apres_credit/12, 2),
+    #     round(part_couple*depenses_apres_credit, 2)
+    #     ],
+    # (f"Dépenses mensuelle réelles part {int(100-100*part_couple)}% couple (€)"): [
+    #     round((1-part_couple)*depenses_apres_credit/12, 2),
+    #     round((1-part_couple)*depenses_apres_credit, 2)
+    #     ]
     }
 df_impots = pd.DataFrame.from_dict(data, orient='index', columns=columns)
 
@@ -211,13 +225,30 @@ c2_hour.text(f"{n_heure_hebdo_maj_50}/semaine")
 st.write("---")
 
 # --- Summary heures
-st.subheader("Heure travaillées mensuelless")
+st.subheader("Heure travaillées par mois")
 st.dataframe(df_heures, use_container_width=True)
 
 # --- Summary salaires
 st.subheader("Salaires mensuels")
 st.dataframe(df_salaires, use_container_width=True)
 
+# Détails des cotisations et aides
+columns = ["Montant (€)"]
+data_aides = {
+    "Prise en charge CAF des cot.sociales à 50 % plafonnée": round(aide_caf, 2),
+    "Montant de la déduction forfaitaire": round(deduction_forfaitaire, 2),
+    "CMG (pour 1 enfant et revenus du couple > 50.7k€)": round(aide_cmg, 2),
+    "Allocation AMGED Asnières (sur revenus)": round(aide_amged_asnieres, 2),
+    "Allocation AMGED Courbevoie (sur revenus)": round(aide_amged_courbevoie, 2),
+    "Allocation BEBEDOM (sur revenus)": round(aide_bebedom, 2),
+    "Total des aides (hors crédit d'impôt)": round(aides, 2),
+    "Cotisations sociales": round(cotisations_sociales, 2),
+    "Reste à payer après aides": round(cotisations_sociales-aides, 2),
+}
+df_aides = pd.DataFrame.from_dict(data_aides, orient='index', columns=columns)
+st.subheader("Détails des cotisations et aides")
+st.dataframe(df_aides, use_container_width=True)
+
 # Summary dépenses et impots
-st.subheader("Dépenses totales pour Co-famille 1 (nous)")
+st.subheader("Dépenses totales pour Co-famille 1")
 st.dataframe(df_impots, use_container_width=True)
